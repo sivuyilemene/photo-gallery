111e5e720659307ec2db81e717254b34fe5bf6ac
Sivuyile Mene <smene98@gmail.com>
2024-05-07 11:48:46 +0200

    added delete photo functionality

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -86,10 +86,28 @@ export function usePhotoGallery() {
           filepath: fileName,
           webviewPath: photo.webPath,
         };
-      };
+      }
+    };
+
+    const deletePhoto = async (photo: UserPhoto) => {
+      // Remove this photo from the Photos reference data array
+      const newPhotos = photos.filter((p) => p.filepath !== photo.filepath);
+    
+      // Update photos array cache by overwriting the existing photo array
+      Preferences.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
+    
+      // delete photo file from filesystem
+      const filename = photo.filepath.substr(photo.filepath.lastIndexOf('/') + 1);
+      await Filesystem.deleteFile({
+        path: filename,
+        directory: Directory.Data,
+      });
+      setPhotos(newPhotos);
+    };
     return {
         photos,
         takePhoto,
+        deletePhoto,
     };
 }
 
