2e8653874136b1340746b6030fa190d97326b70b
Sivuyile Mene <smene98@gmail.com>
2024-05-07 11:53:18 +0200

    implemented the delete photo functionality

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -30,23 +30,6 @@ export function usePhotoGallery() {
             // Web platform only: Load the photo as base64 data
             photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
           }
-        setPhotos(photosInPreferences);
-      };
-      loadSaved();
-    }, []);
-
-    useEffect(() => {
-      const loadSaved = async () => {
-        const { value } = await Preferences.get({ key: PHOTO_STORAGE });
-        const photosInPreferences = (value ? JSON.parse(value) : []) as UserPhoto[];
-    
-        for (let photo of photosInPreferences) {
-          const file = await Filesystem.readFile({
-            path: photo.filepath,
-            directory: Directory.Data,
-          });
-          // Web platform only: Load the photo as base64 data
-          photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
         }
         
         setPhotos(photosInPreferences);
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -15,10 +15,12 @@ import {
   import { camera, trash, close } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
-import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import { useState } from 'react';
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
 
 const Tab2: React.FC = () => {
-  const {photos, takePhoto} = usePhotoGallery();
+  const {photos, takePhoto, deletePhoto} = usePhotoGallery();
+  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
   return (
     <IonPage>
       <IonHeader>
@@ -33,7 +35,7 @@ const Tab2: React.FC = () => {
           <IonRow>
             {photos.map((photo, index) =>(
               <IonCol size="6" key={photo.filepath}>
-                <IonImg src={photo.webviewPath}/>
+                <IonImg onClick={() => setPhotoToDelete(photo)} src={photo.webviewPath}/>
               </IonCol>
             ))}
           </IonRow>
@@ -43,6 +45,28 @@ const Tab2: React.FC = () => {
           <IonIcon icon={camera}></IonIcon>
         </IonFabButton>
         </IonFab>
+        <IonActionSheet
+          isOpen={!!photoToDelete}
+          buttons={[
+            {
+              text: 'Delete',
+              role: 'destructive',
+              icon: trash,
+              handler: () => {
+                if (photoToDelete) {
+                  deletePhoto(photoToDelete);
+                  setPhotoToDelete(undefined);
+                }
+              },
+            },
+            {
+              text: 'Cancel',
+              icon: close,
+              role: 'cancel',
+            },
+          ]}
+          onDidDismiss={() => setPhotoToDelete(undefined)}
+        />
       </IonContent>
     </IonPage>
   );
