9c385ac531f4774a5b037119ccf21dbfd79d599e
Sivuyile Mene <smene98@gmail.com>
2024-05-07 09:15:30 +0200

    added loading functionality

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -35,7 +35,23 @@ export function usePhotoGallery() {
       loadSaved();
     }, []);
 
-
+    useEffect(() => {
+      const loadSaved = async () => {
+        const { value } = await Preferences.get({ key: PHOTO_STORAGE });
+        const photosInPreferences = (value ? JSON.parse(value) : []) as UserPhoto[];
+    
+        for (let photo of photosInPreferences) {
+          const file = await Filesystem.readFile({
+            path: photo.filepath,
+            directory: Directory.Data,
+          });
+          // Web platform only: Load the photo as base64 data
+          photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+        }
+        setPhotos(photosInPreferences);
+      };
+      loadSaved();
+    }, []);
 
     const takePhoto = async () => {
 
